1ba0b8e6f9d4828c6d6bf9b37e75d04184153d2b
YgorCFR <ygor.rolim96@gmail.com>
2019-10-12 19:21:01 -0300

    Ajustando timestamps e relacionamentos

--- models/hpp.js
+++ models/hpp.js
@@ -9,13 +9,15 @@ module.exports = (sequelize, DataTypes) => {
     fatores_risco: DataTypes.INTEGER,
     dac_previa: DataTypes.INTEGER,
     comorbidades: DataTypes.INTEGER
-  }, {});
+  }, {
+    timestamps: false
+  });
   hpp.associate = function(models) {
     // associations can be defined here
     hpp.belongsTo(models.fatores_risco, {foreignKey: 'fatores_risco', as: 'fatores_risco_hpp'});
     hpp.belongsTo(models.dac_previa, {foreignKey: 'dac_previa', as: 'dac_previa_hpp'});
     hpp.belongsTo(models.comorbidades, {foreignKey: 'comorbidades', as: 'comorbidades_hpp'});
-    hpp.hasMany(models.exame, {as: 'hpp_exame'});
+    hpp.hasMany(models.exames, {as: 'hpp_exame'});
   };
   return hpp;
 };
