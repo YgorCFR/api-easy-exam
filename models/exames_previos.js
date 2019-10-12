8a8e433b2296c9a3cbaec5d60e8891bc3f544b74
YgorCFR <ygor.rolim96@gmail.com>
2019-10-12 19:19:00 -0300

    Ajustando timestamps e relacionamento de exames_previos com exame

--- models/exames_previos.js
+++ models/exames_previos.js
@@ -10,14 +10,16 @@ module.exports = (sequelize, DataTypes) => {
     te: DataTypes.INTEGER,
     cm: DataTypes.INTEGER,
     eco: DataTypes.INTEGER
-  }, {});
+  }, {
+    timestamps: false
+  });
   exames_previos.associate = function(models) {
     // associations can be defined here
     exames_previos.belongsTo(models.cat, {foreignKey: 'cat', as: 'cat_exames_previos'});
     exames_previos.belongsTo(models.te, {foreignKey: 'te', as: 'te_exames_previos'});
     exames_previos.belongsTo(models.cm, {foreignKey: 'cm', as: 'cm_exames_previos'});
     exames_previos.belongsTo(models.eco, {foreignKey: 'eco', as: 'eco_exames_previos'});
-    exames_previos.hasMany(models.exame, {foreignKey:'exames_previos' ,as: 'exames_previos_exames'})
+    exames_previos.hasMany(models.exames, {foreignKey:'exames_previos' ,as: 'exames_previos_exames'})
   };
   return exames_previos;
 };
