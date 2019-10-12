3585f5652dca390def241b1655909b976611bbcb
YgorCFR <ygor.rolim96@gmail.com>
2019-10-12 19:23:13 -0300

    Ajustando autoincrement e timestamps

--- models/te.js
+++ models/te.js
@@ -3,11 +3,13 @@ module.exports = (sequelize, DataTypes) => {
   const te = sequelize.define('te', {
     id: {
       type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
+      primaryKey: true
+      //autoIncrement: false
     },
     descricao: DataTypes.STRING
-  }, {});
+  }, {
+    timestamps: false
+  });
   te.associate = function(models) {
     // associations can be defined here
     te.hasMany(models.exames_previos, {as: 'exames_previos_te'})
